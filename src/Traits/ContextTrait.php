10316ddcd78ee3fdbc2ca3ba6908827a30789f6c
Jerome <jerome@metabolism.fr>
2018-09-26 07:10:07 -0700

    fix home url

--- src/Traits/ContextTrait.php
+++ src/Traits/ContextTrait.php
@@ -114,7 +114,7 @@ Trait ContextTrait
 			'language'           => $blog_language,
 			'languages'          => $languages,
 			'is_admin'           => current_user_can('manage_options'),
-			'home_url'           => home_url(),
+			'home_url'           => home_url('/'),
 			'search_url'         => get_search_link(),
 			'privacy_policy_url' => get_privacy_policy_url(),
 			'maintenance_mode'   => wp_maintenance_mode()
